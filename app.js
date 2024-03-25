0495df9be509c4b22d0ea402a5d874bb6096b976
HowardKiat <buisnesskiat123@gmail.com>
2024-03-25 16:50:20 +0800

    upd

---
+++ app.js
@@ -0,0 +1,35 @@
+// required packages
+const express = require("express");
+const fetch = require("node-fetch");
+
+require("dotenv").config();
+
+// create express server
+const app = express();
+
+// server port number
+const PORT = process.env.PORT || 3000;
+
+// set template engine
+app.set("view engine", "ejs");
+app.set(express.static("public"));
+
+// parse html data for POST request
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Halo Za Warudo")
+})
+
+app.post("/", (req, res) => {
+    
+})
+
+// start server
+app.listen(PORT, () =>{
+    console.log(`Server started on PORT: ${PORT}`);
+
+})
