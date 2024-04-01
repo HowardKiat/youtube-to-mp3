66b4f50f5c009183cc0118ff27c8c891373d708e
HowardKiat <buisnesskiat123@gmail.com>
2024-04-02 00:25:15 +0800

    fixed bugs for response

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ const app = express();
 // server port number
 const PORT = process.env.PORT || 3000;
 
-// starting the server
-app.listen(PORT, () => {
-    console.log(`Server Started port ${PORT}`);
-
-})
-
 // parse html data for POST requests
 app.use(express.urlencoded({
     extended: true
@@ -27,12 +21,28 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.render("index")
 
-})
+});
 
-app.post("/", (req, res) => {
+app.post("/convert-mp3", async (req, res) => {
+    const videoLink = req.body.videoLink;
+    if(
+        videoLink === undefined || 
+        videoLink === "" || 
+        videoLink === null
 
-})
+    ){
+        return res.render("index", { success: false, message: "Please Insert a Video Link"});       
+    }else{
+
+    }
+});
 
 // set template engines
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// starting the server
+app.listen(PORT, () => {
+    console.log(`Server Started port ${PORT}`);
+
+})
